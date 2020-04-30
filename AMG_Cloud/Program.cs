f59f20c915e6cf670b6dfb1adf2fb2846da86e43
ShigemoriHakura <hakura@loli.ren>
2020-04-30 23:50:39 +0800

    [sync] 导入0.1版本

---
+++ AMG_Cloud/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace AMG_Cloud
+{
+    public static class Globle
+    {
+        public static string APPName = "AMG_Cloud_Server";
+        public static string APPVersion = "Alpha 0.1";
+        public static int ModelNum = 1;
+        public static int IPNum = 0;
+        public static Dictionary<string, string> ModelToIP;
+        public static Dictionary<string, string> IPMessage;
+        public static Dictionary<string, string> RemoteIPMessage;
+
+        public static Dictionary<string, Dictionary<string, string>> ServeIPMessage;
+        //<主机名,<处理后ip,传输信息>>
+        public static Dictionary<string, int> ServeIPMessageTime;
+        //主机名, 更新时间
+
+    }
+
+    class Program
+    {
+
+        private static SocketManager myP2PServer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Globle.APPName + " 运行版本:" + Globle.APPVersion);
+            Globle.APPVersion = "21212";
+            Globle.ServeIPMessage = new Dictionary<string, Dictionary<string, string>>();
+            Globle.ServeIPMessageTime = new Dictionary<string, int>();
+
+            myP2PServer = new SocketManager();
+            myP2PServer.P2PServerStart();
+
+            var ms = 1000 / 60;
+            System.Timers.Timer pTimer = new System.Timers.Timer(ms);
+            pTimer.Elapsed += pTimer_Elapsed;
+            pTimer.AutoReset = true;
+            pTimer.Enabled = true;
+
+            Console.Read();
+            myP2PServer.P2PServerStop();
+        }
+
+        private static void pTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                if (myP2PServer.P2PServerStatus == true)
+                {
+                    myP2PServer.SendBinaryToClients();
+                }
+            }
+            catch { }
+        }
+
+    }
+}
---
+++ AMG_Cloud/SocketManager.cs
@@ -0,0 +1,128 @@
+﻿using NetworkSocket.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AMG_Cloud
+{
+    class SocketManager
+    {
+        private NetworkSocket.TcpListener P2Plistener;
+        public bool P2PServerStatus = false;
+        public void P2PServerStart()
+        {
+            try
+            {
+                P2Plistener = new NetworkSocket.TcpListener();
+                //P2Plistener.Use<HttpMiddleware>();
+                P2Plistener.Use<WebSocketServerMiddleware>();
+                P2Plistener.UsePlug<CustomWSServerPlug>();
+                P2Plistener.Start(1314);
+                this.P2PServerStatus = true;
+                Console.WriteLine("[WSS]本地服务器已经启动");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[WSS]本地服务器发生错误 " + ex.Message + " : " + ex.StackTrace);
+            }
+        }
+
+        public void P2PServerStop()
+        {
+            this.P2PServerStatus = false;
+            P2Plistener.Dispose();
+            Console.WriteLine("[WSS]本地服务器已经关闭");
+        }
+
+        public void SendBinaryToClients()
+        {
+            //Console.WriteLine(JsonConvert.SerializeObject(Globle.ServeIPMessage));
+            var webSocketSessions = P2Plistener.SessionManager.FilterWrappers<WebSocketSession>();
+            foreach (var item in webSocketSessions)
+            {
+                Thread thread = new Thread(new ParameterizedThreadStart(SendToClient));//创建线程
+
+                thread.Start(item);
+            }
+            /*
+                
+            */
+        }
+
+        public void SendToClient(object ssession)
+        {
+            try
+            {
+                var item = (WebSocketSession)ssession;
+                //var uip = ((System.Net.IPEndPoint)item.RemoteEndPoint).Address.ToString();
+                var uuip = item.RemoteEndPoint.ToString();
+                var rIPMessage = new Dictionary<string, string>();
+                var aServeIPMessage = ObjectCopier.Clone(Globle.ServeIPMessage);
+                //Console.WriteLine(JsonConvert.SerializeObject(aServeIPMessage));
+                aServeIPMessage.Remove(uuip);
+                //Console.WriteLine(JsonConvert.SerializeObject(aServeIPMessage));
+                foreach (KeyValuePair<string, Dictionary<string, string>> kvp in aServeIPMessage)
+                {
+                    foreach (KeyValuePair<string, string> kkvp in kvp.Value)
+                    {
+                        if (rIPMessage.ContainsKey(kkvp.Key))
+                        {
+                            rIPMessage[kkvp.Key] = kkvp.Value;
+                        }
+                        else
+                        {
+                            rIPMessage.Add(kkvp.Key, kkvp.Value);
+                        }
+                    }
+                }
+                var aa = new { keyboardAttached = new ArrayList(), ipMessage = rIPMessage };
+                byte[] byteArray = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(aa));
+                //Console.WriteLine(JsonConvert.SerializeObject(aa));
+                item.SendBinary(byteArray);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[WSS]发生错误：" + ex.Message + " | " + ex.StackTrace);
+            }
+        }
+
+    }
+}
+
+/*
+ *  try
+            {
+                var item = (WebSocketSession)ssession;
+                var uip = ((System.Net.IPEndPoint)item.RemoteEndPoint).Address.ToString();
+                var uuip = item.RemoteEndPoint.ToString();
+                var rIPMessage = new Dictionary<string, string>();
+                var aServeIPMessage = ObjectCopier.Clone(Globle.ServeIPMessage);
+                //var aServeIPMessage = Globle.ServeIPMessage;
+                aServeIPMessage.Remove(uip);
+                foreach (KeyValuePair<string, Dictionary<string, string>> kvp in aServeIPMessage)
+                {
+                    foreach (KeyValuePair<string, string> kkvp in kvp.Value)
+                    {
+                        if (rIPMessage.ContainsKey(kkvp.Key))
+                        {
+                            rIPMessage[kkvp.Key] = kkvp.Value;
+                        }
+                        else
+                        {
+                            rIPMessage.Add(kkvp.Key, kkvp.Value);
+                        }
+                    }
+                }
+                var aa = new { keyboardAttached = new ArrayList(), ipMessage = rIPMessage };
+                byte[] byteArray = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(aa));
+                item.SendBinary(byteArray);
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine("[WSS]发生错误：" + ex.Message + " | " + ex.StackTrace);
+            }
+ * */
